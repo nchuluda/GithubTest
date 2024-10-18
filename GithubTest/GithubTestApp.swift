1f90bc24fe07eed84a9d41eaa4b6577637c9991c
Nathan Chuluda <nchuluda@gmail.com>
2024-10-18 11:35:20 -0400

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Nathan on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
